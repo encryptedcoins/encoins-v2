31b8f7cbd8ef45f22612fd28eba3e1a3d9d26067
ENCOINS <111982001+encryptedcoins@users.noreply.github.com>
2025-06-06 09:16:22 +0200

    Add files via upload

--- UI/branding.js
+++ UI/branding.js
@@ -10,6 +10,8 @@ export const BRANDING = {
   },
   // Logo (set to empty string for no logo)
   logoUrl: "logo.svg",
+  logoAlt: "UTxO Accumulator Logo",
+  networkName: "Preprod",
   // Main style variables
   styles: {
     primaryColor: "#fff",
